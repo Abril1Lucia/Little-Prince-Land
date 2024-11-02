bbe5394e5261725ecfbd583e1ec8d7ed4fd55ffd
Abril1Lucia <abrilcastellanoslopez@gmail.com>
2024-11-01 22:19:27 -0500

    segunda actualizacion

--- src/models/Images.model.js
+++ src/models/Images.model.js
@@ -5,17 +5,16 @@ import mongoose from "mongoose";
 //plantilla de los datos para solicitar guardar la base de datos we
 
 
-const productSchema = new mongoose.Schema({
-    Image:{type: String, required: true},
+const imageSchema = new mongoose.Schema({
+    image:{type: String, required: true},
     name: {type: String, required: true},
-    category: {type: String},
-    price: {type: Number, required: true},
-    stock: {type: Number, required: true},
-    isAvailable: {type: Boolean} //o verdadero o falso we
+    description: {type: String, required: true},
+    technique:{type: String, required: true},
+    category: {type: String, required: true}
 }); 
 
 //decirle a la base de datos q creee una coleccion en esquema anterior
 //el primer parametro es el name de la coleccion
 //el segundo parametro es la estructura de datos we
 
-export const productModel = mongoose.model ('product', productSchema);
+export const imageModel = mongoose.model ('image', imageSchema);
--- src/models/admin.models.js
+++ src/models/admin.models.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 
 
 
-const userSchema = new mongoose.Schema ({
+const adminSchema = new mongoose.Schema ({
     image:{type: String, required: true},
     fullname: {type: String, required:true},
     email: {type: String, required:true, unique:true},
     password: {type: String, required:true},
-    phone: {type: Number, required: true}
+    role: {type: String, default: 'admin'}
 });
 
-export const userModel = mongoose.model('user', userSchema);
+export const adminModel = mongoose.model('admin', adminSchema);
